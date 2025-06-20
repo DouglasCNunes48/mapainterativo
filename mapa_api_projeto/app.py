2709bbec7ec8c600cd89485bde065c0ee20f6252
DouglasCNunes48 <douglas.cnunes48@gmail.com>
2025-06-20 15:09:29 -0300

    app.py

--- mapa_api_projeto/app.py
+++ mapa_api_projeto/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
-from mapa_gerador import gerar_mapa_por_coordenadas
+from mapa_gerador import gerar_mapa_por_latlng
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route('/')
 def home():
-    return 'API de Mapas - OK'
+    return 'API de Mapas ativa.'
 
 @app.route('/mapa')
 def mapa():
@@ -15,8 +15,11 @@ def mapa():
     if not lat or not lng:
         return jsonify({'erro': 'Latitude e longitude obrigatórias'}), 400
 
-    mapa_url = gerar_mapa_por_coordenadas(lat, lng)
-    return jsonify({'url': mapa_url})
+    try:
+        mapa_url = gerar_mapa_por_latlng(float(lat), float(lng))
+        return jsonify({'url': mapa_url})
+    except Exception as e:
+        return jsonify({'erro': str(e)}), 500
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(debug=True)
