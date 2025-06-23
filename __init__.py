f1c28305fd5573c51f962838def795c9b3bff4c7
DouglasCNunes48 <douglas.cnunes48@gmail.com>
2025-06-23 17:29:15 -0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 23 16:59:42 2025
+
+@author: douglas.nunes
+"""
+
---
+++ app.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 20 11:33:36 2025
+
+@author: douglas.nunes
+"""
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from mapa_api_projeto import mapa_gerador
+
+app = FastAPI()
+
+class Localizacao(BaseModel):
+    imovel: str
+    latitude: float
+    longitude: float
+
+@app.get("/")
+def home():
+    return {"mensagem": "API de geração de mapa funcionando."}
+
+@app.post("/gerar-mapa")
+def gerar(localizacao: Localizacao):
+    lat, lng = localizacao.latitude, localizacao.longitude
+    imovel = localizacao.imovel
+
+    restaurantes_1km = mapa_gerador.buscar_restaurantes(lat, lng, 1000)
+    melhores = sorted(restaurantes_1km, key=lambda r: (-r.get('rating', 0), -r.get('user_ratings_total', 0)))[:10]
+    melhores_ids = {r.get('place_id') for r in melhores}
+
+    restaurantes_500m = mapa_gerador.buscar_restaurantes(lat, lng, 500)
+    custo_beneficio = mapa_gerador.selecionar_custo_beneficio(restaurantes_500m, melhores_ids)
+
+    mapa_gerador.gerar_mapa_html(imovel, lat, lng, melhores, custo_beneficio)
+    mapa_gerador.publicar_no_github()
+
+    return {
+        "mensagem": "Mapa publicado com sucesso!",
+        "url": "https://douglascnunes48.github.io/mapainterativo/mapa.html"
+ }
